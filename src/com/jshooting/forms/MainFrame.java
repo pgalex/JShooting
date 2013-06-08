3fe5d909c55dad9cfee909ed815a3491fd85f274
Alexander P. <pgalex@mail.ru>
2013-06-08 19:09:54 +0400

    reports

--- src/com/jshooting/forms/MainFrame.java
+++ src/com/jshooting/forms/MainFrame.java
@@ -5,10 +5,19 @@ import com.jshooting.shootingDatabase.ShootingDatabaseFactory;
 import java.awt.Dialog;
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 /**
  * Main frame
@@ -33,9 +42,9 @@ public class MainFrame extends javax.swing.JFrame
 	{
 		initComponents();
 		setTitle("JShooting");
-
+		
 		shootingDatabase = null;
-
+		
 		readUserSettings();
 		File previousDatabaseFile = new File(UserSettings.getInstance().getDatabaseFileName());
 		if (previousDatabaseFile.exists())
@@ -46,7 +55,7 @@ public class MainFrame extends javax.swing.JFrame
 		{
 			shootingDatabase = null;
 		}
-
+		
 		updateWorkingControlsEnable();
 		updateDatabaseFileNameControls();
 	}
@@ -212,6 +221,13 @@ public class MainFrame extends javax.swing.JFrame
     });
 
     jButtonReports.setText("Отчеты ...");
+    jButtonReports.addActionListener(new java.awt.event.ActionListener()
+    {
+      public void actionPerformed(java.awt.event.ActionEvent evt)
+      {
+        jButtonReportsActionPerformed(evt);
+      }
+    });
 
     jButton1.setText("Средства ...");
     jButton1.addActionListener(new java.awt.event.ActionListener()
@@ -306,12 +322,12 @@ public class MainFrame extends javax.swing.JFrame
 			Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
 		}
   }//GEN-LAST:event_formWindowClosing
-
+	
   private void jButtonCreateDatabaseActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_jButtonCreateDatabaseActionPerformed
   {//GEN-HEADEREND:event_jButtonCreateDatabaseActionPerformed
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-
+		
 		int showDialogResult = fileChooser.showSaveDialog(this);
 		if (showDialogResult == JFileChooser.APPROVE_OPTION)
 		{
@@ -328,17 +344,17 @@ public class MainFrame extends javax.swing.JFrame
 				JOptionPane.showMessageDialog(null, "Невозможно создать базу данных: " + ex.getLocalizedMessage(), "Ошибка", JOptionPane.ERROR_MESSAGE);
 				shootingDatabase = null;
 			}
-
+			
 			updateDatabaseFileNameControls();
 			updateWorkingControlsEnable();
 		}
   }//GEN-LAST:event_jButtonCreateDatabaseActionPerformed
-
+	
   private void jButtonOpenDatabaseActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_jButtonOpenDatabaseActionPerformed
   {//GEN-HEADEREND:event_jButtonOpenDatabaseActionPerformed
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-
+		
 		int showDialogResult = fileChooser.showOpenDialog(this);
 		if (showDialogResult == JFileChooser.APPROVE_OPTION)
 		{
@@ -351,12 +367,12 @@ public class MainFrame extends javax.swing.JFrame
 				JOptionPane.showMessageDialog(null, "Невозможно открыть базу данных: " + ex.getLocalizedMessage(), "Ошибка", JOptionPane.ERROR_MESSAGE);
 				shootingDatabase = null;
 			}
-
+			
 			updateDatabaseFileNameControls();
 			updateWorkingControlsEnable();
 		}
   }//GEN-LAST:event_jButtonOpenDatabaseActionPerformed
-
+	
   private void jButtonSportsmansActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_jButtonSportsmansActionPerformed
   {//GEN-HEADEREND:event_jButtonSportsmansActionPerformed
 		EditSportsmansDialog editSportsmansDialog = new EditSportsmansDialog(this, Dialog.ModalityType.APPLICATION_MODAL,
@@ -364,7 +380,7 @@ public class MainFrame extends javax.swing.JFrame
 		editSportsmansDialog.setLocationRelativeTo(this);
 		editSportsmansDialog.setVisible(true);
   }//GEN-LAST:event_jButtonSportsmansActionPerformed
-
+	
   private void jButton1ActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_jButton1ActionPerformed
   {//GEN-HEADEREND:event_jButton1ActionPerformed
 		EditTrainingMethodsDialog editTrainingMethodsDialog = new EditTrainingMethodsDialog(this, Dialog.ModalityType.APPLICATION_MODAL,
@@ -372,7 +388,7 @@ public class MainFrame extends javax.swing.JFrame
 		editTrainingMethodsDialog.setLocationRelativeTo(this);
 		editTrainingMethodsDialog.setVisible(true);
   }//GEN-LAST:event_jButton1ActionPerformed
-
+	
   private void jButtonAddTrainingActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_jButtonAddTrainingActionPerformed
   {//GEN-HEADEREND:event_jButtonAddTrainingActionPerformed
 		AddShootingTrainingDialog addShootingTrainingDialog = new AddShootingTrainingDialog(this, Dialog.ModalityType.APPLICATION_MODAL,
@@ -382,7 +398,7 @@ public class MainFrame extends javax.swing.JFrame
 		addShootingTrainingDialog.setLocationRelativeTo(this);
 		addShootingTrainingDialog.setVisible(true);
   }//GEN-LAST:event_jButtonAddTrainingActionPerformed
-
+	
   private void jButtonTeamsActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_jButtonTeamsActionPerformed
   {//GEN-HEADEREND:event_jButtonTeamsActionPerformed
 		EditTeamsDialog editTeamsDialog = new EditTeamsDialog(this, Dialog.ModalityType.APPLICATION_MODAL,
@@ -390,6 +406,21 @@ public class MainFrame extends javax.swing.JFrame
 		editTeamsDialog.setLocationRelativeTo(this);
 		editTeamsDialog.setVisible(true);
   }//GEN-LAST:event_jButtonTeamsActionPerformed
+	
+  private void jButtonReportsActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_jButtonReportsActionPerformed
+  {//GEN-HEADEREND:event_jButtonReportsActionPerformed
+		/*try
+		{
+			JasperDesign desing = JRXmlLoader.load("sportsmansReport.jrxml");
+			JasperReport report = JasperCompileManager.compileReport(desing);
+			JasperPrint jasperPrint = JasperFillManager.fillReport(report, null, new TestSportsmansJRDataSource(shootingDatabase));
+			JasperViewer.viewReport(jasperPrint);
+		}
+		catch (JRException ex)
+		{
+			Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+		}*/
+  }//GEN-LAST:event_jButtonReportsActionPerformed
 
 	/**
 	 * @param args the command line arguments
