ed758772529cfbb16c297522ac39075fd80df7a4
Alexander P. <pgalex@mail.ru>
2013-07-06 11:51:46 +0400

    Components highlighting refactoring

--- src/com/jshooting/componentsHighlighting/ComponentsHighlighter.java
+++ src/com/jshooting/componentsHighlighting/ComponentsHighlighter.java
@@ -10,17 +10,29 @@ import java.util.Map;
 import javax.swing.Timer;
 
 /**
- * AWT Components highlighter. Smooth blinks component's background between two
- * colors
+ * AWT Components highlighter. Smoothly blinks component's background between
+ * two colors
  *
  * @author pgalex
  */
 public class ComponentsHighlighter
 {
-	private static final int CHANGIND_COLOR_TIMER_IDLE = 100;
+	/**
+	 * Time between color chaging
+	 */
+	public static final int CHANGIND_COLOR_TIMER_IDLE = 100;
+	/**
+	 * Timer using to periodically changing of components color
+	 */
 	private Timer changingColorTimer;
+	/**
+	 * Map of highlighted components and its color changers
+	 */
 	private Map<Component, ColorChanger> highlightingComponents;
 
+	/**
+	 * Create with no components highlighted
+	 */
 	public ComponentsHighlighter()
 	{
 		highlightingComponents = Collections.synchronizedMap(new HashMap<Component, ColorChanger>());
@@ -35,6 +47,11 @@ public class ComponentsHighlighter
 		});
 	}
 
+	/**
+	 * Using to stop highlighting when application exists
+	 *
+	 * @throws Throwable
+	 */
 	@Override
 	protected void finalize() throws Throwable
 	{
@@ -42,6 +59,12 @@ public class ComponentsHighlighter
 		changingColorTimer.stop();
 	}
 
+	/**
+	 * Event color changing timer - means that need to calculate and change to
+	 * next color for all highlighted components
+	 *
+	 * @param ev
+	 */
 	private void changingColorTimerActionPerformed(ActionEvent ev)
 	{
 		for (Map.Entry<Component, ColorChanger> entry : highlightingComponents.entrySet())
@@ -54,7 +77,19 @@ public class ComponentsHighlighter
 		}
 	}
 
-	// blinkingTime - врем€, за которое компонент должен изменить цвет от начального до highlightingColor и обратно
+	/**
+	 * Start highlighting of component
+	 *
+	 * @param component component to highlight. Its background color will be
+	 * overriden. Must be not null
+	 * @param highlightColor color for highlighted state of component. Must be not
+	 * null
+	 * @param blinkingTime time in which need to change components color from
+	 * normal state to highlighted and back. Must be more than
+	 * CHANGIND_COLOR_TIMER_IDLE constant
+	 * @throws IllegalArgumentException component is null; highlightColor is null;
+	 * blinkingTime less or equals CHANGIND_COLOR_TIMER_IDLE
+	 */
 	public void startComponentHightlighing(Component component, Color highlightColor, int blinkingTime) throws IllegalArgumentException
 	{
 		if (component == null)
@@ -70,6 +105,11 @@ public class ComponentsHighlighter
 			throw new IllegalArgumentException("blinkingTime must be more than " + CHANGIND_COLOR_TIMER_IDLE);
 		}
 
+		if (highlightingComponents.containsKey(component))
+		{
+			return; // already highlighted
+		}
+
 		ColorChanger colorChanger = new ColorChanger(component.getBackground(), highlightColor, blinkingTime, CHANGIND_COLOR_TIMER_IDLE);
 		highlightingComponents.put(component, colorChanger);
 
@@ -79,7 +119,15 @@ public class ComponentsHighlighter
 		}
 	}
 
-	public void stopComponentHighlighting(Component component)
+	/**
+	 * Stop highlighted of component
+	 *
+	 * @param component component which highlighting need to stop. Background
+	 * color of components set to color that was while starting of component
+	 * highlighting. Must be not null
+	 * @throws IllegalArgumentException component is null
+	 */
+	public void stopComponentHighlighting(Component component) throws IllegalArgumentException
 	{
 		if (component == null)
 		{
