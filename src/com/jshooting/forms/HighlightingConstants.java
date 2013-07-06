518e4713847b8c52a1a91c992480e33b5e875fbc
Alexander P. <pgalex@mail.ru>
2013-07-06 12:09:06 +0400

    Components highlighting refactoring

--- src/com/jshooting/forms/HighlightingConstants.java
+++ src/com/jshooting/forms/HighlightingConstants.java
@@ -9,6 +9,12 @@ import java.awt.Color;
  */
 public class HighlightingConstants
 {
+	/**
+	 * Time while components changes color to highlighted state and back
+	 */
 	public static final int BLINKING_TIME = 1200;
+	/**
+	 * Highlighting color which means "accepted" or "good" option
+	 */
 	public static final Color GOOD_HIGHLIGHT_COLOR = new Color(160, 255, 160, 255);
 }
